9a6d5058e80c68f583efad946c26ae96793f667b
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2023-01-26 10:04:25 +0100

    show funzionante

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,8 +1,18 @@
-@extends('layouts.admin')
+{{-- @extends('layouts.admin')
 
 @section('page-content')
     <h1>{{$post->title}}</h1>
     <p>{{$post->content}}</p>
     <span>{{$post->slug}}</span>
     <a style="display:inline-block" href="{{route('admin.posts.index')}}" class="btn btn-primary my-5">Torna alla Dashboard</a>
+@endsection --}}
+
+@extends('layouts.admin')
+
+@section('content')
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->content}}</p>
+    <a href="" class="btn btn-warning my-1 d-inline-block">Modifica</a>
+    <a href="" class="btn btn-danger my-1">Elimina</a>
+    
 @endsection
